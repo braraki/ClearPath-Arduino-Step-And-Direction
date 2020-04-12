ad53cdd6b2366687751fbd3e81f255a7c946a6d6
Brandon Araki <br.araki@gmail.com>
2020-04-12 13:41:37 -0700

    test1 works

---
+++ Arduino Uno/ClearPathStepGen/Examples/test1/test1.ino
@@ -0,0 +1,77 @@
+/*
+  SingleAxisDemo
+  Runs a Teknic ClearPath SDSK or SDHP motor, back and forth
+ 
+ This example code is in the public domain.
+ */
+
+//Import Required libraries
+#include <ClearPathMotorSD.h>
+#include <ClearPathStepGen.h>
+
+// initialize a ClearPathMotorSD Motor
+ClearPathMotorSD X;
+
+//initialize the controller and pass the reference to the motor we are controlling
+ClearPathStepGen machine(&X);
+
+// the setup routine runs once when you press reset:
+void setup()
+{  
+  //Begin Serial Communication // NOTE: If communication lags, consider increasing baud rate
+  Serial.begin(9600);
+  
+//X.attach(9);                //attach motor so Step/B is connected to pin 9
+//X.attach(8,9);              //Direction/A is pin 8, Step/B is pin 9
+X.attach(4,22,2);            //Direction/A is pin 9, Step/B is pin 22, Enable is pin 2
+//X.attach(8,9,6,4);          //Direction/A is pin 8, Step/B is pin 9, Enable is pin 6, HLFB is pin 4
+
+// Set max Velocity.  Parameter can be between 2 and 100,000 steps/sec
+  X.setMaxVel(10000);
+  
+// Set max Acceleration.  Parameter can be between 4000 and 2,000,000 steps/sec/sec
+  X.setMaxAccel(2000000);
+  
+// Enable motor, reset the motor position to 0
+X.enable();
+
+delay(100);
+
+// Set up the ISR to constantly update motor position.  All motor(s) must be attached, and enabled before this function is called.
+machine.Start();
+
+ 
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{  
+  
+ // Move the motor forward 100 steps
+   X.move(-1500);
+   Serial.println("Move Start");
+   
+// wait until the command is finished and The motor's HLFB asserts
+//   while(!X.commandDone()||!X.readHLFB()) //just use command done if not using motor feedback
+//   { }
+   while(!X.commandDone()) //just use command done if not using motor feedback
+   { }
+   long pos = X.getCommandedPosition();
+   Serial.print("commanded position IN: ");
+   Serial.println(pos);
+   delay(1000);
+//   Serial.println("Move hopefully Done");
+  
+// Move the motor backwards 100 steps
+   X.move(1500);
+   Serial.println("Negative Move Begins");
+   
+// wait until the command is finished and The motor's HLFB asserts  
+   while(!X.commandDone()) //just use command done if not using motor feedback
+   { }
+   Serial.print("commanded position OU: ");
+   Serial.println(X.getCommandedPosition());
+   delay(1000);
+   Serial.println("Move hopefully Done");
+   
+}
